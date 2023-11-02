3728a1fc2e31c6aeae0c4c34d97662daf4634825
Milagros <mchoque@madisa.es>
2023-11-02 11:01:55 +0100

    Agregar archivos de proyecto.

---
+++ MonitorCocinaBackOffice.Module/BusinessObjects/ORMDataModel1Code/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace MonitorCocinaBackOffice.Module.BusinessObjects
+{
+
+	public partial class Department
+	{
+		public Department(Session session) : base(session) { }
+		public override void AfterConstruction() { base.AfterConstruction(); }
+	}
+
+}
